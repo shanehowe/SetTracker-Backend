4094da71715bb61e57e05873a79abb108ecc3b2a
Shane Howe <shanehowe100@hotmail.com>
2024-03-10 23:28:20 +0000

    Update sign-in route to get provider from body

--- function_app.py
+++ function_app.py
@@ -2,6 +2,26 @@ import azure.functions as func
 
 app = func.FunctionApp()
 
-@app.route(route="signin", auth_level=func.AuthLevel.ANONYMOUS, methods=("POST"))
+@app.route(route="auth/signin", auth_level=func.AuthLevel.ANONYMOUS, methods=["POST"])
 def sign_in(req: func.HttpRequest) -> func.HttpResponse:
-    return func.HttpResponse("Hello, World! This is a sign in page")
+    body = req.get_json()
+
+    try:
+        provider = body["provider"]
+    except KeyError:
+        return func.HttpResponse(
+            "Please provide a provider",
+            status_code=400
+        )
+    
+    match provider:
+        case "apple":
+            return func.HttpResponse(
+                "Apple Sign In",
+                status_code=200
+            )
+        case _:
+            return func.HttpResponse(
+                "Unsupported provider",
+                status_code=400
+            )
