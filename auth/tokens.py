5892328a38b4fce3f67567d1c3f5fdc96cb173f7
Shane Howe <shanehowe100@hotmail.com>
2024-03-11 21:54:04 +0000

    Add JWT token encoding and decoding functions

---
+++ auth/tokens.py
@@ -0,0 +1,14 @@
+import jwt
+import datetime
+from utils.date_utils import add_days_to_date
+
+
+def encode_jwt(payload: dict, secret: str, algorithm: str = "HS256") -> str:
+    today = datetime.datetime.now()
+    twenty_four_hours = add_days_to_date(today, 1)
+    updated_payload = {**payload, "exp": twenty_four_hours}
+    return jwt.encode(updated_payload, secret, algorithm=algorithm)
+
+
+def decode_jwt(token: str, secret: str, algorithm: str = "HS256") -> dict:
+    return jwt.decode(token, secret, algorithms=[algorithm])
