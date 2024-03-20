827782e85cf5d6632f099c24e2f815dd0dee07b9
Shane Howe <shanehowe100@hotmail.com>
2024-03-20 22:50:05 +0000

    Add authentication and user service to sign_in function

--- function_app.py
+++ function_app.py
@@ -1,27 +1,35 @@
 import azure.functions as func
+import json
+from pydantic import ValidationError
+
+from app.service.user_service import UserService
+from app.models.auth_models import AuthRequest
 
 app = func.FunctionApp()
 
 @app.route(route="auth/signin", auth_level=func.AuthLevel.ANONYMOUS, methods=["POST"])
 def sign_in(req: func.HttpRequest) -> func.HttpResponse:
-    body = req.get_json()
-
+    user_service = UserService()
     try:
-        provider = body["provider"]
-    except KeyError:
+        body = req.get_json()
+    except ValueError:
         return func.HttpResponse(
-            "Please provide a provider",
+            body="Invalid request. Please provide json",
             status_code=400
         )
+
+    try:
+        auth_data = AuthRequest(**body)
+    except ValidationError:
+        return func.HttpResponse(
+            body="Invalid request data. Please provide identity_token and oAuth provider"
+        )
     
-    match provider:
-        case "apple":
-            return func.HttpResponse(
-                "Apple Sign In",
-                status_code=200
-            )
-        case _:
-            return func.HttpResponse(
-                "Unsupported provider",
-                status_code=400
-            )
+    authenticated_user = user_service.authenticate(auth_data)
+    if authenticated_user is None:
+        return func.HttpResponse(body="Failed to authenticate user.")
+    
+    return func.HttpResponse(
+        body=json.dumps({"token": authenticated_user})
+    )
+
