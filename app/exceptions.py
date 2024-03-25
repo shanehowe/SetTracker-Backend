f9482420af77e9e3aa0152a2050ea8f1a52e0936
Shane Howe <shanehowe100@hotmail.com>
2024-03-25 23:01:39 +0000

    Add AuthenticationException to replace InvalidAuthCredentials

--- app/exceptions.py
+++ app/exceptions.py
@@ -2,5 +2,6 @@ class UnsupportedProviderException(Exception):
     """Raised when oAuth provider is not supported"""
 
 
-class InvalidAuthCredentialsException(Exception):
-    """Raised when credentials given cannot be authenticated"""
+class AuthenticationException(Exception):
+    def __init__(self, message: str = "Authentication failed"):
+        super().__init__(message)
