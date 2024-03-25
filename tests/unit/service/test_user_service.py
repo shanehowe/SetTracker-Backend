74bbf6cf0a6f759de9fc6767b1cf461cdf75cfe2
Shane Howe <shanehowe100@hotmail.com>
2024-03-25 23:28:32 +0000

    Add test ensuring exception raise when given an invalid provider

--- tests/unit/service/test_user_service.py
+++ tests/unit/service/test_user_service.py
@@ -1,7 +1,10 @@
 import pytest
-from unittest.mock import MagicMock, ANY
+from unittest.mock import MagicMock
 from app.service.user_service import UserService
 from app.models.user_models import BaseUser, UserInDB
+from app.models.auth_models import AuthRequest
+from app.exceptions import AuthenticationException
+import app.auth.tokens
 
 
 @pytest.fixture
@@ -29,3 +32,15 @@ def test_create_user(user_service, mock_user_data_access):
     args, _ = mock_user_data_access.create_user.call_args
     assert args[0].email == "example@email.com"
     assert args[0].provider == "apple"
+
+
+def test_authenticate_raises_exception_when_given_invalid_provider(user_service, mock_user_data_access, monkeypatch):
+    monkeypatch.setattr(
+        app.auth.tokens,
+        "decode_and_verify_token",
+        MagicMock(side_effect=app.auth.tokens.UnsupportedProviderException)
+    )
+
+    with pytest.raises(AuthenticationException, match="oAuth provider not supported"):
+        user_service.authenticate(AuthRequest(token="token", provider="invalid_provider"))
+    assert not mock_user_data_access.get_user_by_email.called
