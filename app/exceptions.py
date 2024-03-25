2304283431d917c70b5bb1f653989603d11e1b77
Shane Howe <shanehowe100@hotmail.com>
2024-03-25 00:21:27 +0000

    add InvalidCredentialsException

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,2 +1,6 @@
 class UnsupportedProviderException(Exception):
     """Raised when oAuth provider is not supported"""
+
+
+class InvalidAuthCredentialsException(Exception):
+    """Raised when credentials given cannot be authenticated"""
