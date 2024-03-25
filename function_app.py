de4c7f24d33ca5ecaeedbda5d3c63bcc3f410bf4
Shane Howe <shanehowe100@hotmail.com>
2024-03-25 00:21:42 +0000

    change function app to be ASGI app

--- function_app.py
+++ function_app.py
@@ -1,35 +1,5 @@
 import azure.functions as func
-import json
-from pydantic import ValidationError
 
-from app.service.user_service import UserService
-from app.models.auth_models import AuthRequest
+from app.main import fast_app
 
-app = func.FunctionApp()
-
-@app.route(route="auth/signin", auth_level=func.AuthLevel.ANONYMOUS, methods=["POST"])
-def sign_in(req: func.HttpRequest) -> func.HttpResponse:
-    user_service = UserService()
-    try:
-        body = req.get_json()
-    except ValueError:
-        return func.HttpResponse(
-            body="Invalid request. Please provide json",
-            status_code=400
-        )
-
-    try:
-        auth_data = AuthRequest(**body)
-    except ValidationError:
-        return func.HttpResponse(
-            body="Invalid request data. Please provide identity_token and oAuth provider"
-        )
-    
-    authenticated_user = user_service.authenticate(auth_data)
-    if authenticated_user is None:
-        return func.HttpResponse(body="Failed to authenticate user.")
-    
-    return func.HttpResponse(
-        body=json.dumps({"token": authenticated_user})
-    )
-
+app = func.AsgiFunctionApp(app=fast_app, http_auth_level=func.AuthLevel.ANONYMOUS)
