39a41fb16ca889c55e663a37ee989131c334edc0
Shane Howe <shanehowe100@hotmail.com>
2024-03-29 15:57:00 +0000

    add WorkoutFolder models

---
+++ app/models/workout_folder_models.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class BaseWorkoutFolder(BaseModel):
+    class Config:
+        populate_by_name = True
+
+
+class WorkoutFolderInDB(BaseWorkoutFolder):
+    id: str
+    name: str
+    user_id: str
+    exercises: list[str]
+
+
+class WorkoutFolderInRequest(BaseWorkoutFolder):
+    name: str
+    exercises: Optional[list[str]] = None
