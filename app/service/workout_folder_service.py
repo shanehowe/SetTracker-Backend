24ffa5e81cb2c0bfa542c1ea35dc9973c641b7ef
Shane Howe <shanehowe100@hotmail.com>
2024-03-31 22:21:38 +0100

    add business logic for retrieving a folder

--- app/service/workout_folder_service.py
+++ app/service/workout_folder_service.py
@@ -1,6 +1,8 @@
 from uuid import uuid4
 from app.data_access.workout_folder import WorkoutFolderDataAccess
 from app.models.workout_folder_models import WorkoutFolderInDB, WorkoutFolderInRequest
+from app.exceptions import UnauthorizedAccessException
+from azure.cosmos.exceptions import CosmosResourceNotFoundError
 
 
 class WorkoutFolderService:
@@ -9,6 +11,15 @@ class WorkoutFolderService:
         workout_folder_data_access: WorkoutFolderDataAccess = WorkoutFolderDataAccess(),
     ) -> None:
         self.workout_folder_data_access = workout_folder_data_access
+
+    def get_folder_by_id(self, folder_id: str, user_requesting_folder: str):
+        try:
+            retrieved_folder = self.workout_folder_data_access.get_folder_by_id(folder_id)
+        except CosmosResourceNotFoundError:
+            return None
+        if retrieved_folder.user_id != user_requesting_folder:
+            raise UnauthorizedAccessException("This folder does not belong to the user")
+        return retrieved_folder
 
     def get_users_workout_folders(self, user_id: str) -> list[WorkoutFolderInDB]:
         return self.workout_folder_data_access.get_users_workout_folders(user_id)
