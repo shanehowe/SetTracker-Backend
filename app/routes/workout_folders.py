e2641a08680b1c6f6820d712498bad99ce4bece0
Shane Howe <shanehowe100@hotmail.com>
2024-03-31 13:51:43 +0100

    add route handler for creating workout folders

--- app/routes/workout_folders.py
+++ app/routes/workout_folders.py
@@ -5,6 +5,7 @@ from app.service.workout_folder_service import (
     WorkoutFolderService,
 )
 from app.dependencies import get_current_user
+from app.models.workout_folder_models import WorkoutFolderInRequest
 
 
 workout_folder_router = APIRouter(prefix="/workout-folders", tags=["workout folders"])
@@ -23,3 +24,16 @@ def get_users_folders(
             detail="Invalid token payload", status_code=status.HTTP_400_BAD_REQUEST
         )
     return workout_folder_service.get_users_workout_folders(user_id)
+
+
+@workout_folder_router.post("/")
+def create_workout_folder(
+    folder_to_create: WorkoutFolderInRequest,
+    workout_folder_service: Annotated[
+        WorkoutFolderService, Depends(get_workout_folder_service)
+    ],
+    decoded_token: dict = Depends(get_current_user),
+):
+    return workout_folder_service.create_workout_folder(
+        folder_to_create, decoded_token.get("id")
+    )
