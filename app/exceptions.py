eed02086de1e5f2dfa7bb9560d1bc8746ae37cc1
Shane Howe <shanehowe100@hotmail.com>
2024-03-31 22:19:34 +0100

    add exception for when requested entity does not belong to a user requesting it

--- app/exceptions.py
+++ app/exceptions.py
@@ -5,3 +5,8 @@ class UnsupportedProviderException(Exception):
 class AuthenticationException(Exception):
     def __init__(self, message: str = "Authentication failed"):
         super().__init__(message)
+
+
+class UnauthorizedAccessException(Exception):
+    def __init__(self, message="Unauthorized access"):
+        super().__init__(message)
