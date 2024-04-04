23851106974df06781ac0ae281bfa68b4b3941dd
Shane Howe <shanehowe100@hotmail.com>
2024-04-04 19:00:51 +0100

    extend tests for updating folder and lint file

--- tests/unit/service/test_workout_folder_service.py
+++ tests/unit/service/test_workout_folder_service.py
@@ -3,7 +3,10 @@ from unittest.mock import MagicMock
 from azure.cosmos.exceptions import CosmosResourceNotFoundError
 from app.exceptions import UnauthorizedAccessException
 from app.service.workout_folder_service import WorkoutFolderService
-from app.models.workout_folder_models import WorkoutFolderInDB, WorkoutFolderInRequest
+from app.models.workout_folder_models import (
+    WorkoutFolderInDB,
+    WorkoutFolderInUpdate,
+)
 
 
 @pytest.fixture
@@ -18,7 +21,9 @@ def workout_folder_service(mock_workout_folder_data_access):
     )
 
 
-def test_get_workout_folder_by_id(mock_workout_folder_data_access, workout_folder_service):
+def test_get_workout_folder_by_id(
+    mock_workout_folder_data_access, workout_folder_service
+):
     mock_workout_folder_data_access.get_folder_by_id.return_value = WorkoutFolderInDB(
         id="123", user_id="123", name="test folder", exercises=[]
     )
@@ -32,7 +37,9 @@ def test_get_workout_folder_by_id(mock_workout_folder_data_access, workout_folde
 def test_get_workout_folder_by_id_returns_none_when_folder_not_found(
     mock_workout_folder_data_access, workout_folder_service
 ):
-    mock_workout_folder_data_access.get_folder_by_id.side_effect = CosmosResourceNotFoundError()
+    mock_workout_folder_data_access.get_folder_by_id.side_effect = (
+        CosmosResourceNotFoundError()
+    )
     folder = workout_folder_service.get_folder_by_id("123", "123")
     assert folder is None
 
@@ -47,7 +54,9 @@ def test_get_workout_folder_by_id_raises_unauthorized_exception(
         workout_folder_service.get_folder_by_id("123", "456")
 
 
-def test_get_users_workout_folders(mock_workout_folder_data_access, workout_folder_service):
+def test_get_users_workout_folders(
+    mock_workout_folder_data_access, workout_folder_service
+):
     mock_workout_folder_data_access.get_users_workout_folders.return_value = [
         WorkoutFolderInDB(id="123", user_id="123", name="test folder", exercises=[])
     ]
@@ -57,3 +66,51 @@ def test_get_users_workout_folders(mock_workout_folder_data_access, workout_fold
     assert folders[0].user_id == "123"
     assert folders[0].name == "test folder"
     assert folders[0].exercises == []
+
+
+def test_update_workout_folder(mock_workout_folder_data_access, workout_folder_service):
+    mock_workout_folder_data_access.get_folder_by_id.return_value = WorkoutFolderInDB(
+        id="123", user_id="123", name="test folder", exercises=[]
+    )
+    mock_workout_folder_data_access.update_workout_folder.return_value = (
+        WorkoutFolderInDB(
+            id="123", user_id="123", name="updated folder", exercises=["exercise1"]
+        )
+    )
+    updated_folder = workout_folder_service.update_workout_folder(
+        "123",
+        WorkoutFolderInUpdate(name="updated folder", exercises=["exercise1"]),
+        "123",
+    )
+    assert updated_folder.id == "123"
+    assert updated_folder.user_id == "123"
+    assert updated_folder.name == "updated folder"
+    assert updated_folder.exercises == ["exercise1"]
+
+
+def test_update_workout_folder_raises_unauthorized_exception(
+    mock_workout_folder_data_access, workout_folder_service
+):
+    mock_workout_folder_data_access.get_folder_by_id.return_value = WorkoutFolderInDB(
+        id="123", user_id="123", name="test folder", exercises=[]
+    )
+    with pytest.raises(UnauthorizedAccessException):
+        workout_folder_service.update_workout_folder(
+            "123",
+            WorkoutFolderInUpdate(name="updated folder", exercises=["exercise1"]),
+            "456",
+        )
+
+
+def test_update_workout_folder_returns_none_when_resource_doesnt_exist(
+    mock_workout_folder_data_access, workout_folder_service
+):
+    mock_workout_folder_data_access.get_folder_by_id.side_effect = (
+        CosmosResourceNotFoundError()
+    )
+
+    assert workout_folder_service.update_workout_folder(
+        "123",
+        WorkoutFolderInUpdate(name="updated folder", exercises=["exercise1"]),
+        "123",
+    ) is None
