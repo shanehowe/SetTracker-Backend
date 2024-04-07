d7a9b5845d490134c2f22e6bda22770bcba21951
Shane Howe <shanehowe100@hotmail.com>
2024-04-07 22:05:29 +0100

    create models for exercises

---
+++ app/models/exercises_models.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, ConfigDict
+
+
+class BaseExercise(BaseModel):
+    model_config = ConfigDict(populate_by_name=True)
+
+
+class ExerciseInDB(BaseExercise):
+    id: str
+    name: str
+    body_parts: list[str]
+    creator: str
+
+
+class ExerciseInCreate(BaseExercise):
+    id: str
+    name: str
