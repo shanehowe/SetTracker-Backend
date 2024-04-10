1c2dee2dfcf42df450914b695800a25213d46037
Shane Howe <shanehowe100@hotmail.com>
2024-04-10 21:32:27 +0100

    Add exercises_router to app/routes/exercises.py

---
+++ app/routes/exercises.py
@@ -0,0 +1,30 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException, status
+
+from app.dependencies import get_current_user
+from app.exceptions import ExerciseAlreadyExistsException
+from app.models.exercises_models import ExerciseInCreate
+from app.service.exercise_service import ExerciseService, get_exercise_service
+
+exercises_router = APIRouter(prefix="/exercises")
+
+
+@exercises_router.get("/")
+def get_all_exercises(
+    exercise_service: Annotated[ExerciseService, Depends(get_exercise_service)],
+    decoded_token: dict = Depends(get_current_user),
+):
+    return exercise_service.get_system_and_user_exercises(decoded_token["id"])
+
+
+@exercises_router.post("/", status_code=status.HTTP_201_CREATED)
+def create_custom_exercise(
+    exercise: ExerciseInCreate,
+    exercise_service: Annotated[ExerciseService, Depends(get_exercise_service)],
+    decoded_token: dict = Depends(get_current_user),
+):
+    try:
+        return exercise_service.create_custom_exercise(exercise, decoded_token["id"])
+    except ExerciseAlreadyExistsException as e:
+        raise HTTPException(detail=str(e), status_code=status.HTTP_400_BAD_REQUEST)
