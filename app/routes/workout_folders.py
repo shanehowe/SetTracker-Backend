4212f4a071fd89f39d32238041802f03a8946b39
Shane Howe <shanehowe100@hotmail.com>
2024-04-10 21:29:43 +0100

    Refactor workout_folder_router in workout_folders.py

--- app/routes/workout_folders.py
+++ app/routes/workout_folders.py
@@ -1,16 +1,17 @@
-from fastapi import APIRouter, Depends, HTTPException, status
 from typing import Annotated
-from app.service.workout_folder_service import (
-    get_workout_folder_service,
-    WorkoutFolderService,
-)
+
+from fastapi import APIRouter, Depends, HTTPException, status
+
 from app.dependencies import get_current_user
 from app.exceptions import UnauthorizedAccessException
 from app.models.workout_folder_models import (
     WorkoutFolderInRequest,
     WorkoutFolderInUpdate,
 )
-
+from app.service.workout_folder_service import (
+    WorkoutFolderService,
+    get_workout_folder_service,
+)
 
 workout_folder_router = APIRouter(prefix="/workout-folders", tags=["workout folders"])
 
@@ -20,9 +21,9 @@ def get_users_folders(
     workout_folder_service: Annotated[
         WorkoutFolderService, Depends(get_workout_folder_service)
     ],
-    decoded_token: dict = Depends(get_current_user),
+    decoded_token: dict[str, str] = Depends(get_current_user),
 ):
-    user_id = decoded_token.get("id")
+    user_id = decoded_token["id"]
     return workout_folder_service.get_users_workout_folders(user_id)
 
 
@@ -54,10 +55,10 @@ def create_workout_folder(
     workout_folder_service: Annotated[
         WorkoutFolderService, Depends(get_workout_folder_service)
     ],
-    decoded_token: dict = Depends(get_current_user),
+    decoded_token: dict[str, str] = Depends(get_current_user),
 ):
     return workout_folder_service.create_workout_folder(
-        folder_to_create, decoded_token.get("id")
+        folder_to_create, decoded_token["id"]
     )
 
 
@@ -68,11 +69,11 @@ def update_workout_folder(
     workout_folder_service: Annotated[
         WorkoutFolderService, Depends(get_workout_folder_service)
     ],
-    decoded_token: dict = Depends(get_current_user),
+    decoded_token: dict[str, str] = Depends(get_current_user),
 ):
     try:
         updated_folder = workout_folder_service.update_workout_folder(
-            folder_id, folder_to_update, user_id=decoded_token.get("id")
+            folder_id, folder_to_update, user_id=decoded_token["id"]
         )
     except ValueError as e:
         raise HTTPException(detail=str(e), status_code=status.HTTP_400_BAD_REQUEST)
@@ -92,14 +93,14 @@ def delete_workout_folder(
     workout_folder_service: Annotated[
         WorkoutFolderService, Depends(get_workout_folder_service)
     ],
-    decoded_token: dict = Depends(get_current_user),
+    decoded_token: dict[str, str] = Depends(get_current_user),
 ):
     try:
-        is_success = workout_folder_service.delete_workout_folder(folder_id, decoded_token.get("id"))
+        is_success = workout_folder_service.delete_workout_folder(
+            folder_id, decoded_token["id"]
+        )
     except ValueError as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
     except UnauthorizedAccessException as e:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
-    return {
-        "detail": is_success
-    }
+    return {"detail": is_success}
