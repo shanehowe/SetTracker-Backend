b5cb189633c43b7df705177a4f3232a9a74b2bee
Shane Howe <shanehowe100@hotmail.com>
2024-04-14 21:24:02 +0100

    Add routes for sets and include set router in main.py

--- app/main.py
+++ app/main.py
@@ -3,8 +3,10 @@ from fastapi import FastAPI
 from app.routes.authentication import auth_router
 from app.routes.exercises import exercises_router
 from app.routes.workout_folders import workout_folder_router
+from app.routes.sets import set_router
 
 fast_app = FastAPI()
 fast_app.include_router(auth_router)
 fast_app.include_router(workout_folder_router)
 fast_app.include_router(exercises_router)
+fast_app.include_router(set_router)
