a9a7ab88bafc4b110287ed4c1bb89126800d03e6
Shane Howe <shanehowe100@hotmail.com>
2024-04-16 10:27:34 +0100

    Refactor create_set method in sets.py to handle exceptions and return appropriate HTTP status codes

--- app/routes/sets.py
+++ app/routes/sets.py
@@ -1,9 +1,9 @@
-from datetime import datetime
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from app.dependencies import get_current_user
+from app.exceptions import ExerciseDoesNotExistException, UserDoesNotExistException
 from app.models.set_models import SetInCreate
 from app.service.set_service import SetService, get_set_service
 
@@ -19,10 +19,13 @@ def get_users_sets_by_exercise_id(
     return set_service.get_users_sets_by_exercise_id(exercise_id, current_user["id"])
 
 
-@set_router.post("/")
+@set_router.post("/", status_code=status.HTTP_201_CREATED)
 def create_set(
     set_to_create: SetInCreate,
     set_service: Annotated[SetService, Depends(get_set_service)],
     current_user: dict[str, str] = Depends(get_current_user),
 ):
-    pass
+    try:
+        return set_service.create_set(set_to_create, current_user["id"])
+    except (UserDoesNotExistException, ExerciseDoesNotExistException) as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
