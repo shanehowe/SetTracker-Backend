961af91ce98ced11ffcb0a3f501aa8911ba47995
Shane Howe <shanehowe100@hotmail.com>
2024-04-17 22:00:55 +0100

    Refactor sort_set_history function in set_utils.py to improve code readability and maintainability

--- app/utils/set_utils.py
+++ app/utils/set_utils.py
@@ -1,4 +1,6 @@
 import functools
+from copy import deepcopy
+from datetime import datetime
 
 from app.models.set_models import SetGroup, SetInDB
 
@@ -21,3 +23,20 @@ def group_sets_by_date(sets: list[SetInDB]) -> list[SetGroup]:
     return [
         SetGroup(sets=value, date_created=key) for key, value in grouped_dict.items()
     ]
+
+
+def sort_set_history(set_history: list[SetGroup]) -> list[SetGroup]:
+    """
+    Set history in json/dict format is a list of dicts. Each
+    dict has a date_created and sets key which is a list.
+    Firstly this function sorts the top level history by date in reverse
+    chronological order. Then for each dictionary sorts the sets in the same way.
+
+    :param set_history: The data to be sorted
+    :returns SetGroup: In sorted order.
+    """
+    data_for_sort = deepcopy(set_history)
+    for set_group in data_for_sort:
+        set_group.sets.sort(key=lambda x: datetime.fromisoformat(x.date_created))
+    data_for_sort.sort(key=lambda x: datetime.fromisoformat(x.date_created), reverse=True)
+    return data_for_sort
