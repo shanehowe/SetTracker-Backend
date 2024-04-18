5c7ff9ae3b98fcfb6cf96f2f52308f911c785508
Shane Howe <shanehowe100@hotmail.com>
2024-04-18 23:21:57 +0100

    Refactor delete_set method in set_service.py to improve code readability and maintainability

--- app/data_access/set.py
+++ app/data_access/set.py
@@ -6,6 +6,10 @@ class SetDataAccess(BaseDataAccess):
     def __init__(self) -> None:
         super().__init__(container_name="exercise-sets")
 
+    def get_set_by_id(self, set_id) -> SetInDB:
+        item = self.container.read_item(set_id, partition_key=set_id)
+        return SetInDB(**item)
+
     def get_users_sets_by_exercise_id(
         self, exercise_id: str, user_id: str
     ) -> list[SetInDB]:
@@ -22,3 +26,6 @@ class SetDataAccess(BaseDataAccess):
     def create_set(self, set_to_create: SetInDB) -> SetInDB:
         created_set = self.container.create_item(body=set_to_create.model_dump())
         return SetInDB(**created_set)
+
+    def delete_set(self, set_id: str) -> None:
+        self.container.delete_item(set_id, partition_key=set_id)
