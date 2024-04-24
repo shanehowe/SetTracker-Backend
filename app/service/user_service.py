731db82aa82d955a443cb0669bec08fd80de659c
Shane Howe <shanehowe100@hotmail.com>
2024-04-24 23:17:28 +0100

    Update user_service.py to include update_user_preferences method and add corresponding tests

--- app/service/user_service.py
+++ app/service/user_service.py
@@ -5,9 +5,13 @@ from jwt.exceptions import PyJWTError
 
 from app.auth.tokens import decode_and_verify_token, encode_jwt
 from app.data_access.user import UserDataAccess
-from app.exceptions import AuthenticationException, UnsupportedProviderException
+from app.exceptions import (
+    AuthenticationException,
+    EntityNotFoundException,
+    UnsupportedProviderException,
+)
 from app.models.auth_models import AuthRequest
-from app.models.user_models import BaseUser, UserInDB, UserInResponse
+from app.models.user_models import BaseUser, Preferences, UserInDB, UserInResponse
 
 
 class UserService:
@@ -63,6 +67,30 @@ class UserService:
         user_id = str(uuid4())
         user_for_creation = UserInDB(**user.model_dump(), id=user_id)
         return self.user_data_access.create_user(user_for_creation)
+
+    def update_user_preferences(
+        self, preferences: Preferences, user_id: str
+    ) -> Preferences:
+        """
+        Update users preferences.
+        :param updated_preferences: The preferences object with the data to update
+        :param user_id: The ID of the user to preform the operation on.
+        :raises EntityNotFoundException: The user_id does not belong to an existing user.
+        """
+        user_to_update = self.get_user_by_id(user_id)
+        if user_to_update is None:
+            raise EntityNotFoundException(f"No user with ID: {user_id}")
+        # Get the current preferences
+        # add the current preferences into the ones to be updated
+        # if its not already in there.
+        current_preferences = user_to_update.preferences.model_dump()
+        updating_preferences = preferences.model_dump()
+        for key in current_preferences:
+            if updating_preferences.get(key) is None:
+                updating_preferences[key] = current_preferences[key]
+        user_to_update.preferences = Preferences(**updating_preferences)
+        updated_user = self.user_data_access.update_user(user_to_update)
+        return updated_user.preferences
 
 
 def get_user_service() -> UserService:
