cb6d61497e7267f5244ad956cb25ca05ce0c0d2e
Shane Howe <shanehowe100@hotmail.com>
2024-04-27 22:53:25 +0100

    Update user_router to include get_user_preferences endpoint in users.py

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,4 +1,5 @@
 from typing import Annotated
+
 from fastapi import APIRouter, Depends, HTTPException, status
 
 from app.dependencies import get_current_user
@@ -6,15 +7,25 @@ from app.exceptions import EntityNotFoundException
 from app.models.user_models import Preferences
 from app.service.user_service import UserService, get_user_service
 
-
 user_router = APIRouter(prefix="/me", tags=["users"])
+
+
+@user_router.get("/preferences")
+def get_user_preferences(
+    user_service: Annotated[UserService, Depends(get_user_service)],
+    current_user: dict[str, str] = Depends(get_current_user),
+):
+    try:
+        return user_service.get_user_preferences(current_user["id"])
+    except EntityNotFoundException as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
 
 
 @user_router.put("/preferences", status_code=status.HTTP_204_NO_CONTENT)
 def update_preferences(
     preferences: Preferences,
     user_service: Annotated[UserService, Depends(get_user_service)],
-    current_user: dict[str, str] = Depends(get_current_user)
+    current_user: dict[str, str] = Depends(get_current_user),
 ):
     try:
         user_service.update_user_preferences(preferences, current_user["id"])
