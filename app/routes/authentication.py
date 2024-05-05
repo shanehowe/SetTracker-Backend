969b2fce8a823241194395ec2540f57ca7136610
Shane Howe <shanehowe100@hotmail.com>
2024-05-05 22:45:10 +0100

    refactor: Update sign_in and sign_up endpoints to use UserEmailAuthInSignUpAndIn model

--- app/routes/authentication.py
+++ app/routes/authentication.py
@@ -3,14 +3,14 @@ from fastapi.routing import APIRouter
 
 from app.exceptions import AuthenticationException, EntityAlreadyExistsException
 from app.models.auth_models import AuthRequest
-from app.models.user_models import UserInResponse, UserEmailAuthInSignUp
+from app.models.user_models import UserEmailAuthInSignUpAndIn, UserInResponse
 from app.service.user_service import UserService, get_user_service
 
 auth_router = APIRouter(prefix="/auth", tags=["Authentication"])
 
 
 @auth_router.post(
-    "/signin/oAuth", status_code=status.HTTP_200_OK, response_model=UserInResponse
+    "/signin/oauth", status_code=status.HTTP_200_OK, response_model=UserInResponse
 )
 def sign_in_oAuth(
     auth_data: AuthRequest, user_service: UserService = Depends(get_user_service)
@@ -26,14 +26,20 @@ def sign_in_oAuth(
 @auth_router.post(
     "/signin", status_code=status.HTTP_200_OK, response_model=UserInResponse
 )
-def sign_in(user_service: UserService = Depends(get_user_service)):
-    return {"detail": "Working"}
+def sign_in(
+    user_for_sign_in: UserEmailAuthInSignUpAndIn,
+    user_service: UserService = Depends(get_user_service),
+):
+    try:
+        return user_service.authenticate_email_password_auth(user_for_sign_in)
+    except AuthenticationException as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
 
 
 @auth_router.post("/signup")
 def sign_up(
-    user_for_sign_up: UserEmailAuthInSignUp,
-    user_service: UserService = Depends(get_user_service)
+    user_for_sign_up: UserEmailAuthInSignUpAndIn,
+    user_service: UserService = Depends(get_user_service),
 ):
     try:
         return user_service.sign_up_user(user_for_sign_up)
