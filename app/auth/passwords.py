c99c38be64adee76ca797960675b1d4f503c7884
Shane Howe <shanehowe100@hotmail.com>
2024-05-05 22:43:14 +0100

    refactor: Update get_password_hash function to return string instead of bytes

--- app/auth/passwords.py
+++ app/auth/passwords.py
@@ -3,7 +3,7 @@ import bcrypt
 
 def get_password_hash(plain_text_password: str) -> str:
     hashed_bytes = bcrypt.hashpw(plain_text_password.encode(), bcrypt.gensalt())
-    return str(hashed_bytes)
+    return hashed_bytes.decode()
 
 
 def check_password(plain_text_password: str, hashed_password: str) -> bool:
