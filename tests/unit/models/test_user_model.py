c11f5bbb65336c10799c4b8d541c1af4a7559642
Shane Howe <shanehowe100@hotmail.com>
2024-05-14 21:17:47 +0100

    refactor: Update password length validation in UserEmailAuthInSignUpAndIn model

--- tests/unit/models/test_user_model.py
+++ tests/unit/models/test_user_model.py
@@ -1,6 +1,7 @@
+from pydantic import ValidationError
 import pytest
 
-from app.models.user_models import BaseUser
+from app.models.user_models import BaseUser, UserEmailAuthInSignUpAndIn
 
 
 @pytest.mark.parametrize(
@@ -15,3 +16,21 @@ from app.models.user_models import BaseUser
 def test_base_user_transforms_email_to_lowercase_and_strips_whitespace(email):
     user = BaseUser(email=email)
     assert user.email == email.strip().lower()
+
+
+@pytest.mark.parametrize("password", ["12345", "", "123"])
+def test_user_email_auth_raises_ecxeption_when_password_length_less_than_six(password):
+    with pytest.raises(ValueError):
+        UserEmailAuthInSignUpAndIn(email="something@something.com", password=password)
+
+
+@pytest.mark.parametrize("password", ["123456", "1234567", "12345678"])
+def test_user_email_auth_does_not_raise_exception_when_password_length_is_six_or_more(
+    password,
+):
+    try:
+        UserEmailAuthInSignUpAndIn(email="something@something.com", password=password)
+    except ValidationError:
+        pytest.fail(
+            f"ValidationError was raised unexpectedly for password: {password} - Length: {len(password)}"
+        )
