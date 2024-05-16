2e3baaeae2f198aeed622ec6cbec4efdb0b12252
Shane Howe <shanehowe100@hotmail.com>
2024-05-16 16:49:29 +0100

    refactor: Update user deletion in logged_in_client helper function

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -1,10 +1,12 @@
 import uuid
+
 import pytest
+from azure.cosmos.exceptions import CosmosResourceNotFoundError
 from fastapi.testclient import TestClient
 
 from app.auth.passwords import get_password_hash
-from app.main import fast_app
 from app.data_access.user import UserDataAccess
+from app.main import fast_app
 from app.models.user_models import UserInDB
 
 
@@ -34,4 +36,8 @@ def logged_in_client(client: TestClient, user_data_access: UserDataAccess):
     token = response.json()["token"]
     client.headers = {"Authorization": f"Bearer {token}"}
     yield client
-    user_data_access.delete_user(user.id)
+    try:
+        user_data_access.delete_user(user.id)
+    except CosmosResourceNotFoundError:
+        # Another test has already deleted the user
+        pass
